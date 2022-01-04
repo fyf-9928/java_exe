6164873b9daf68b4c1bb8ee62227036916eeeafe
fyf-9928 <84448565+fyf-9928@users.noreply.github.com>
2022-01-04 16:43:53 +0800

    代码1
    第1章例题

---
+++ src/exe1/ex5.java
@@ -0,0 +1,9 @@
+package exe1;
+
+public class ex5 {
+	public static void main(String[] args){
+
+		System.out.println(1/0);
+
+	}
+}
---
+++ src/exe1/ex6.java
@@ -0,0 +1,10 @@
+package exe1;
+
+public class ex6 {
+	public static void main(String[] args){
+
+		System.out.print("Celsius 35 is Fahrenheit degree");
+		System.out.println((9/5)*32+32);
+
+	}
+}
